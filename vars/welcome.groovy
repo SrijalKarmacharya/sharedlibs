5388f8f8d42960fa47094213d5b274e50528f94e
SrijalKarmacharya <86567992+SrijalKarmacharya@users.noreply.github.com>
2021-08-19 09:08:27 +0545

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,16 +1,20 @@
-
+def call(body) {
+  def pipelineParams= [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
  
   pipeline{
     agent any
     stages{
  	   stage('Demo'){
-         steps{
-	        script{
-	       calculator.add(20,30)
-	       calculator.mul(20,30)
-	       } 
-	        }
-     }
+               steps{
+	          script{
+	            calculator.add(20,30)
+	            calculator.mul(20,30)
+	         } 
+	      }
+          }
     }
   }
 }
