b9dce11f83368ffba8d7fc063a7e03f3a2652f14
SrijalKarmacharya <86567992+SrijalKarmacharya@users.noreply.github.com>
2021-08-19 09:06:43 +0545

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,13 +1,14 @@
-def call(name){
- echo "Hey ${name}, How are you?"
+
  
   pipeline{
     agent any
     stages{
  	   stage('Demo'){
          steps{
-	         welcome("Srijal Karmacharya")
-	 	 welcome("Tyler Durden")  
+	        script{
+	       calculator.add(20,30)
+	       calculator.mul(20,30)
+	       } 
 	        }
      }
     }
